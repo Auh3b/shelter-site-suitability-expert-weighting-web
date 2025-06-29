c5987fcfd6d6895e93c37e943f1e980922c62965
Auh3b <ngaiyayec@yahoo.com>
2025-06-29 15:34:33 +0200

    feat: notification toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Toaster } from 'sonner';
 import App from './App.tsx';
 import './index.css';
 import store from './store/index.ts';
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
